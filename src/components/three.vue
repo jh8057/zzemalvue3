6cad9e9a3c07ad096be8eca6acdbdc2aff1b3bac
jh8057 <luckysymbol13@gmail.com>
2022-08-21 23:41:56 +0900

    three-cube

---
+++ src/components/three.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Three.js</h1>
+    <main>
+      <h2>contents</h2>
+      <div class="experience">
+        <canvas class="experience-canvas"></canvas>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import Experience from "@/plugin/three";
+
+export default {
+  setup() {
+    const experience = new Experience(
+      document.querySelector(".experience-canvas")
+    );
+  },
+};
+</script>
+
+<style></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <three-comp />
   </div>
 </template>
+
+<script lang="ts">
+import threeComp from "@/components/three.vue";
+export default {
+  components: {
+    threeComp,
+  },
+};
+</script>
+
+<style></style>
