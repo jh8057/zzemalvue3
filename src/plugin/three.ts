377e68dfc4e0680400540f9c7e89712782e243dd
jh8057 <luckysymbol13@gmail.com>
2022-10-03 01:31:57 +0900

    [three] animation

--- src/plugin/three.ts
+++ src/plugin/three.ts
@@ -135,6 +135,34 @@ export default class Experience {
             }
         );
 
+        let mixer: any;
+        // Load a glTF resource
+        loader.load(
+            // resource URL
+            '/blender/robot.gltf',
+            // called when the resource is loaded
+            function (gltf) {
+                const model = gltf.scene;
+                scene.add(model);
+                mixer = new THREE.AnimationMixer(model);
+                const clips = gltf.animations;
+
+                const clip = THREE.AnimationClip.findByName(clips, 'goFront');
+                const action = mixer.clipAction(clip);
+                action.play();
+
+                const clock = new THREE.Clock();
+
+                function animate() {
+                    requestAnimationFrame(animate);
+                    if (mixer) mixer.update(clock.getDelta());
+
+                    renderer.render(scene, camera);
+                }
+
+                animate();
+            }
+        );
         // function animate() {
         //     requestAnimationFrame(animate);
 
@@ -149,51 +177,3 @@ export default class Experience {
         // animate();
     }
 }
-
-// export default class Experience {
-//     canvas: any;
-
-//     constructor(canvas: any) {
-//         // if(Experience.instance){
-//         //     return Experience.instance
-//         // }
-//         // Experience.instance = this
-
-//         this.canvas = canvas;
-
-//         //basic setting
-//         const scene = new THREE.Scene();
-//         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-//         const renderer = new THREE.WebGLRenderer({ antialias: true });
-//         renderer.setPixelRatio(window.devicePixelRatio);
-//         renderer.setSize(window.innerWidth, window.innerHeight);
-//         document.body.appendChild(renderer.domElement);
-
-//         //make cube
-//         const geometry = new THREE.BoxGeometry(1, 1, 1);
-//         const material = new THREE.MeshBasicMaterial({ color: 0x44a88 });
-//         const cube = new THREE.Mesh(geometry, material);
-//         scene.add(cube);
-//         // scene.background = new THREE.Color(0xffffff)
-
-//         //light
-//         const light = new THREE.PointLight(0xff0000, 1, 100);
-//         light.position.set(50, 50, 50);
-//         scene.add(light);
-
-//         camera.position.z = 5;
-
-//         function animate() {
-//             requestAnimationFrame(animate);
-
-//             //animation
-//             cube.rotation.x += 0.01;
-//             cube.rotation.y += 0.01;
-
-//             renderer.render(scene, camera);
-//         }
-
-//         animate();
-//     }
-// }
